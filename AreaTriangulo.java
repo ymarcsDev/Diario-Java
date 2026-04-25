2b5c2ffaa7d5f5406e19b867aff1a65d804de0d5
ymarcsDev <ymarcosvalente1984@gmail.com>
2026-04-24 21:57:53 -0300

    Implementação de cálculo de área de triângulo com POO
    Neste commit, evoluí o código de cálculo de área de triângulo para utilizar os conceitos iniciais de Programação Orientada a Objetos (POO).

---
+++ AreaTriangulo.java
@@ -0,0 +1,41 @@
+package condicionais;
+
+import entities.triangle;
+
+import java.util.Scanner;
+
+public class AreaTriangulo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        triangle x,y;
+        x=new triangle();
+        y=new triangle();
+
+        System.out.println("Enter the measures of triangle X: ");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+        System.out.println("Enter the measures of triangle Y: ");
+        y.a = sc.nextDouble();
+        y.b= sc.nextDouble();
+        y.c= sc.nextDouble();
+
+        double p = (x.a + x.b + x.c) / 2.0;
+        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+
+        p = (y.a + y.b + y.c) / 2.0;
+
+        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+
+        if (areaX > areaY) {
+            System.out.println("Larger area: X");
+        } else {
+            System.out.println("Larger area: Y");
+        }
+
+        }
+    }
---
+++ triangle.java
@@ -0,0 +1,7 @@
+package entities;
+
+public class triangle {
+    public double a;
+    public double b;
+    public double c;
+}

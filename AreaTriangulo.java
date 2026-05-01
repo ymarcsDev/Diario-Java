b01d704b5f550052f1a8b725cb9c04ed080fb57b
ymarcsDev <ymarcosvalente1984@gmail.com>
2026-05-01 16:15:48 -0300

    Cálculo de área de triângulos usando POO
    Implementação de uma classe Triangle para representar a entidade e uma classe principal para processar as áreas e comparar os resultados.

--- AreaTriangulo.java
+++ AreaTriangulo.java
@@ -8,9 +8,9 @@ public class AreaTriangulo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        triangle x,y;
-        x=new triangle();
-        y=new triangle();
+        triangle x, y;
+        x = new triangle();
+        y = new triangle();
 
         System.out.println("Enter the measures of triangle X: ");
         x.a = sc.nextDouble();
@@ -18,15 +18,11 @@ public class AreaTriangulo {
         x.c = sc.nextDouble();
         System.out.println("Enter the measures of triangle Y: ");
         y.a = sc.nextDouble();
-        y.b= sc.nextDouble();
-        y.c= sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2.0;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-
-        p = (y.a + y.b + y.c) / 2.0;
-
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+        double areaX = x.area();
+        double areaY = y.area();
 
         System.out.printf("Triangle X area: %.4f%n", areaX);
         System.out.printf("Triangle Y area: %.4f%n", areaY);
@@ -37,5 +33,5 @@ public class AreaTriangulo {
             System.out.println("Larger area: Y");
         }
 
-        }
     }
+}
